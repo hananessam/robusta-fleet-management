c949c261e82b423b2aa826133f8ec0c7c506e438
hananessam <hanan.essam228@gmail.com>
2023-08-30 21:39:18 +0300

    filter trips by start and end station

--- app/Repositories/Trip/TripRepository.php
+++ app/Repositories/Trip/TripRepository.php
@@ -15,7 +15,23 @@ class TripRepository extends BaseRepository implements TripInterface
 
     public function models($request)
     {
+        // filter by
         $models = $this->model->where(function ($query) use ($request) {
+            // start station
+            if($request->start_station) {
+                $query->whereHas('stops', function ($query) use ($request) {
+                    $query->where('city_from_id', $request->start_station);
+                });
+            }
+
+            // end staion
+            if($request->end_station) {
+                $query->whereHas('stops', function ($query) use ($request) {
+                    $query->where('city_to_id', $request->end_station);
+                });
+            }
+
+            // start date
             if($request->date_time_from) {
                 $query->whereDate('date_time_from', '>=', $request->date_time_from)
                     ->whereHas('available_seats');
