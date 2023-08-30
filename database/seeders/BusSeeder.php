1fc8ba9f5c742a65d55eddfa416494a4a3a6035a
hananessam <hanan.essam228@gmail.com>
2023-08-30 21:58:05 +0300

    refine bus seeder

--- database/seeders/BusSeeder.php
+++ database/seeders/BusSeeder.php
@@ -15,7 +15,7 @@ class BusSeeder extends Seeder
     {
         $busses = Bus::factory()->count(5)->create();
         foreach ($busses as $bus) {
-            for ($i = 1; $i <= 30; $i++) {
+            for ($i = 1; $i <= 12; $i++) {
                 $bus->seats()->create(["number" => $i]);
             }
         }
